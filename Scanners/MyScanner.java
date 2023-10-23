e94229783cadb9c6afdb21aa6770b21193e05c6c
Rohan Huynh <rohan.huynh@mrjaffesclass.com>
2023-10-23 11:47:25 -0700

    Scanners Start

---
+++ Scanners/MyScanner.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class MyScanner
+{
+
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            int str = scanner.nextInt();
+            System.out.println(str * 3);
+        }
+    }
+
+    public static void main(String[] args) {
+        MyScanner myScanner = new MyScanner();
+        myScanner.run();
+    }
+}
