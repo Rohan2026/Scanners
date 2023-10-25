84cf95147072cb2550bc64b8204dc82124302710
Rohan Huynh <rohan.huynh@mrjaffesclass.com>
2023-10-25 11:39:41 -0700

    Progress day 3

--- Scanners/Scanner2.java
+++ Scanners/Scanner2.java
@@ -6,13 +6,14 @@ public class Scanner2
     public void run() {
         Scanner scanner = new Scanner(System.in).useDelimiter(",");
         while (true) {
+           if (scanner.hasNext()) {
             String str = scanner.next();
             System.out.println(str);
         }
     }
-
+  }
     public static void main(String[] args) {
-        Scanner2 myScanner = new Scanner2();
-        myScanner.run();
+        Scanner2 Scanner2 = new Scanner2();
+        Scanner2.run();
     }
 }
