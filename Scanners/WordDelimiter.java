cf58734439fc4545b7d9bc7c01b19e95f0b4baab
Rohan Huynh <rohan.huynh@mrjaffesclass.com>
2023-11-02 11:38:18 -0700

    Scanners Done

--- Scanners/WordDelimiter.java
+++ Scanners/WordDelimiter.java
@@ -1,17 +1,20 @@
 import java.util.Scanner;
 
-public class WordDelimiter {
-    public static void main(String[] args) {
+public class WordDelimiter
+{
+
+    public void run() {
         Scanner scanner = new Scanner(System.in);
-        
-        scanner.useDelimiter(",");
-        
-        String inputLine = scanner.nextLine();
-        
-        String[] words = inputLine.split(",");
-    
-        for (String word : words) {
-            System.out.println(word);
+        while (true) {
+           if (scanner.hasNext()) {
+            scanner = scanner.useDelimiter(",|\n"); 
+            String str = scanner.next();
+            System.out.println(str);
+           }
         }
+    }
+    public static void main(String[] args) {
+        WordDelimiter WordDelimiter = new WordDelimiter();
+        WordDelimiter.run();
     }
 }
